e586ac1132b5e1240e168d2acf3c9be9dfb1f292
David <davejs136@gmail.com>
2020-01-03 14:30:24 -0400

    First commit

---
+++ modules/__init__.py
@@ -0,0 +1,2 @@
+from .data import json_template
+from .data import file_path
---
+++ modules/data.py
@@ -0,0 +1,12 @@
+json_template = [
+  {
+    'name': 'Test',
+    'password': 'test1234'
+  },
+  {
+    'name': 'Example',
+    'password': 'example1234'
+  }
+]
+
+file_path = 'data.json'
